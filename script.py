00527319e44a6d22dd2ffcc2e5c1c352b952125f
Ethan Gough <dante1498@gmail.com>
2021-06-01 15:53:22 +0100

    Add login

--- script.py
+++ script.py
@@ -9,3 +9,10 @@ request_token_url = 'https://login.salesforce.com/services/oauth2/token'
 access_token_url = 'https://login.salesforce.com/services/oauth2/token'
 redirect_url = ''
 authorize_url = 'https://login.salesforce.com/services/oauth2/authorize' #?response_type=token&client_id='+consumer_key+'&redirect_uri='+redirect_uri
+
+query = cgi.FieldStorage()
+req = None
+
+if 'login' in query:
+    print "Location: https://login.salesforce.com/services/oauth2/authorize?response_type=code&client_id="+consumer_key+"&redirect_uri="+redirect_uri
+    print
